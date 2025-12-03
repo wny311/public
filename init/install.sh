28537b8cb514597fc81d875ab0732989582476be
wny311 <wang@gitlab.com>
2025-12-03 10:54:08 +0800

    update cilium install IPAM

--- init/install.sh
+++ init/install.sh
@@ -194,7 +194,9 @@ function cni_cilium_install {
   tar xvf cilium-linux-${CLI_ARCH}.tar.gz -C /usr/local/bin
   rm cilium-linux-${CLI_ARCH}.tar.gz
 
-  cilium install --wait
+  cilium install --set ipam.operator.clusterPoolIPv4PodCIDRList="10.244.0.0/16" \
+    --set ipv4NativeRoutingCIDR="10.244.0.0/16" \
+    --set ipam="cluster-pool"  --wait
 
   cilium status
 
